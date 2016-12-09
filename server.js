e84ea02e7fa3e3cecb1d9fae9b512f1d3f98f8d2
u6025499 <apided.kajonnatiyut@thomsonreuters.com>
2016-12-09 09:57:07 +0700

    initiate project

---
+++ server.js
@@ -0,0 +1,24 @@
+// call the needed packages
+var express = require('express');        // call express
+var app = express();	//define App using express
+
+var port = 8080;  // set port
+
+// ROUTES FOR OUR API
+// ===============================================================================
+var router = express.Router();
+
+// Defult testing route (accessed by GET http://localhost:8080/)
+app.get('/', function(req, res) {
+	res.json({ message: 'Welcome' });
+	console.log('Welcome');	
+});
+
+// REGISTER ROUTES
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// ===============================================================================
+app.listen(port);
+console.log('Start on port ' + port);
